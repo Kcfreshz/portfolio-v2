e51edbe56fe31a7b013fff35dcbddc6763c21201
kcfreshz <kckc176@gmail.com>
2025-06-20 08:41:23 +0100

    Fixed the resume section to have both view and downloadable button

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,23 +1,37 @@
 import { motion } from "framer-motion";
 import { FaCloudDownloadAlt } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
 function Resume() {
   return (
     <ScrollReveal>
       <section id="resume" className="pl-10 md:pl-[400px] p-4 md:py-24">
         <div className="container max-w-3xls mx-auto">
-          <h2 className="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3 ">
+          <h2 className="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 pb-3">
             Resume
           </h2>
-          <div className="inline-flex items-center px-4 py-2 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition-colors">
-            <FaCloudDownloadAlt className="mr-2" />
+
+          <div className="flex gap-4 px-4">
+            {/* View Resume Button */}
             <a
               href="/Kelechukwu_Awoke_resume.pdf"
+              target="_blank"
               rel="noopener noreferrer"
-              // target="_blank"
-              download="kelechukwu_awoke_resume.pdf"
+              className="inline-flex items-center px-4 py-2 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition-colors"
             >
+              <FaEye className="mr-2" />
               View Resume
+            </a>
+
+            {/* Download Resume Button */}
+            <a
+              href="/Kelechukwu_Awoke_resume.pdf"
+              download="Kelechukwu_Awoke_Resume.pdf"
+              rel="noopener noreferrer"
+              className="inline-flex items-center px-4 py-2 bg-blue-950 text-white rounded-lg hover:bg-blue-900 transition-colors"
+            >
+              <FaCloudDownloadAlt className="mr-2" />
+              Download Resume
             </a>
           </div>
         </div>
